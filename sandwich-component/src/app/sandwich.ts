a5c120624ba9d7ddc3c3762491154e394fb802fe
Chris Christian <clyde769@revature.net>
2022-05-26 06:27:50 -0500

    Customer should be able to select a few options from menu, sandwich, bread, sauce, seasoning and add to local host

---
+++ sandwich-component/src/app/sandwich.ts
@@ -0,0 +1,36 @@
+export interface Sandwich {
+    cart_id: number,
+    cost: number,
+    cart_date: Date,
+    sandwiches: [
+        {
+            "sandwich_id": number,
+            "bread": {
+                "id": number,
+                "name": string,
+                "price": number
+            },
+            "meat": {
+                "id": number,
+                "name": string,
+                "price": number
+            },
+            "vegetable": {
+                "id": number,
+                "name": string,
+                "price": number
+            },
+            "sauce": {
+                "id": number,
+                "name": string,
+                "price": number
+            },
+            "seasoning": {
+                "id": number,
+                "name": string,
+                "price": number
+            }
+        }
+    ]
+}
+
