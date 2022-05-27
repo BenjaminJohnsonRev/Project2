7757bcf4ac3f163f6981cab897e34ce6ca877ca6
HaruyaMaeda <hmaeda022@gmail.com>
2022-05-27 02:20:29 -0500

    I think I got it to connect to AWS RDS

--- BackEnd/src/main/java/org/example/controller/SandwichController.java
+++ BackEnd/src/main/java/org/example/controller/SandwichController.java
@@ -2,19 +2,26 @@ package org.example.controller;
 
 
 import org.example.entity.menu.Sandwich;
+import org.example.entity.menu.ingredients.*;
+import org.example.services.IngredientService;
 import org.example.services.SandwichService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/sandwiches")
 public class SandwichController {
     @Autowired
     SandwichService sandwichService;
 
+    @Autowired
+    IngredientService ingredientService;
+
     @PostMapping
+    @ResponseBody
     public Sandwich add_sandwich(@RequestBody Sandwich sandwich){
         return sandwichService.add_sandwich(sandwich);
     }
@@ -33,7 +40,6 @@ public class SandwichController {
     public Sandwich update_sandwich(@RequestBody Sandwich sandwich){
         return sandwichService.update_sandwich(sandwich);
     }
-
 
     @DeleteMapping("/{id_to_delete}")
     public void delete_sandwich(@PathVariable("id_to_delete") long id){
