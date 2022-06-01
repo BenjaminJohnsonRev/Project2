cf693d612ddf2529c10f79aab7ae0fb5d4ff3ba1
nickjolshansky <103455498+nickjolshansky@users.noreply.github.com>
2022-06-01 17:12:13 -0400

    minor changers to complaints

--- FrontEnd/Valentina/megabites-app/src/app/complaint/complaint.component.ts
+++ FrontEnd/Valentina/megabites-app/src/app/complaint/complaint.component.ts
@@ -46,6 +46,8 @@ export class ComplaintComponent implements OnInit {
     this.complaintService.addComplaint(this.complaint).subscribe((complaint:Complaint)=>{
       console.log(complaint);
     })
+
+    this.ngOnInit();
   }
 
 }
