d758193085f842e0f54540f2d74dfdfb9f6db676
HaruyaMaeda <hmaeda022@gmail.com>
2022-06-06 15:08:09 -0500

    Additional user stories implemented

--- FrontEnd/Valentina/megabites-app/src/app/cart.ts
+++ FrontEnd/Valentina/megabites-app/src/app/cart.ts
@@ -1,9 +1,11 @@
 import { Timestamp } from "rxjs";
+import { Sandwich } from "./sandwich";
 
 export interface Cart{
     cart_id?:Number,
     customer_id?:Number,
     employee_id:Number,
     cost_sum:Number,
-    cart_date?:Date
+    cart_date?:Date,
+    sandwiches?:Sandwich[]
 }
